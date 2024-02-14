fa102678c085ae10cf7badb0ac1ac7129c8cdb11
danieloynes <danielskole07@gmail.com>
2024-02-14 12:24:16 +0100

    Gjettelek

---
+++ index.js
@@ -0,0 +1,25 @@
+//Oppgave 1
+/*
+var tall = Math.round(Math.random()*100);
+let svar = window.prompt("Skriv et tall mellom 1 og 100");
+
+while (true) {
+    
+if (svar == tall) {
+    alert("Riktig svar")
+} else {
+    alert("Feil svar, prøv igjen")
+    let svar = window.prompt("Skriv et tall mellom 1 og 100");
+}
+}
+*/
+
+//Oppgave 2
+/*
+let i = 0;
+
+while (i < 10) {
+  console.log("123456789");
+  i++;
+}
+*/
